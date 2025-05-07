661527745bdb6ec9a3b97543580e01935b306c86
Luis <luismarfran08@gmail.com>
2025-05-07 01:25:46 -0600

    ejemplo Doscstring

--- 2 funciones, estructuras de datos y excepciones/04-notebook.py
+++ 2 funciones, estructuras de datos y excepciones/04-notebook.py
@@ -1,7 +1,25 @@
-dictionary = {'1° Trimestre':9.5,'2° Trimestre':8,'3° Trimestre':7}
+"""dictionary = {'1° Trimestre':9.5,'2° Trimestre':8,'3° Trimestre':7}
 
 sumaDictionary = sum(dictionary.values())
 
 promedio = round((sumaDictionary/len(dictionary)), 1)
 
-print(promedio)
+print(promedio)"""
+
+def sumar(a: int, b: int) -> int:
+    '''Función que suma dos números.
+
+    a: int
+        Primer número a sumar.
+    b: int
+        Segundo número a sumar.
+    return: int
+        La suma de a y b.
+    '''
+    return a + b
+
+sum = sumar(1, 7)
+
+print(sum)
+
+help(sumar)
