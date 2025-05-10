b08eee8c0c7ccca44229641fb893fe1c35dc6653
Luis <luismarfran08@gmail.com>
2025-05-10 03:23:46 -0600

    Update 02-ejercicios.py

--- 02-ejercicios.py
+++ 02-ejercicios.py
@@ -20,9 +20,10 @@ print(tercer_tupla)"""
 # crea un código para generar una lista de tuplas en la que cada tupla tenga el primer elemento como la posición del 
 # nombre en la lista original y el segundo elemento siendo el propio nombre.
 
-lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo']
+"""lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo']
 
 lista_tupla = [(i, nombre) for i, nombre in enumerate(lista)]
 
-print(lista_tupla)
+print(lista_tupla)"""
 
+# 
