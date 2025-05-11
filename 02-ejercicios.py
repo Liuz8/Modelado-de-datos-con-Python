f734ba2039e5a660de3d194d6efbb4646bcb08a2
Luis <luismarfran08@gmail.com>
2025-05-10 23:45:45 -0600

    Update 02-ejercicios.py

--- 02-ejercicios.py
+++ 02-ejercicios.py
@@ -47,3 +47,29 @@ diccionario = {mes:g for mes, g in zip(meses, gasto)}
 
 print(diccionario)"""
 
+# 6 - Una tienda tiene una base de datos con la información de venta de cada representante y de cada año y necesita 
+# filtrar solo los datos del año 2022 con ventas mayores a 6000. La tienda proporcionó una muestra con solo las columnas de los 
+# años y los valores de venta para que puedas ayudar a realizar la filtración de los datos a través de un código:
+# Crea una lista usando la comprensión de listas para filtrar los valores de 2022 que sean mayores a 6000.
+
+"""ventas = [('2023', 4093), ('2021', 4320), ('2021', 5959), ('2022', 8883), ('2023', 9859), ('2022', 5141), ('2022', 7688), 
+          ('2022', 9544), ('2023', 4794), ('2021', 7178), ('2022', 3030), ('2021', 7471), ('2022', 4226), ('2022', 8190), 
+          ('2021', 9680), ('2022', 5616)]
+
+ventas2 = [i for i in ventas if (i[0] == '2022') and (i[1] >= 6000)]
+
+print(ventas2)
+
+print(ventas[0])"""
+
+# 7 - Una clínica analiza datos de pacientes y almacena el valor numérico de la glucosa en una base de datos y le gustaría 
+# etiquetar los datos de la siguiente manera:
+
+# Glucosa igual o inferior a 70: 'Hipoglicemia'
+# Glucosa entre 70 y 99: 'Normal'
+# Glucosa entre 100 y 125: 'Alterada'
+# Glucosa superior a 125: 'Diabetes'
+# La clínica proporcionó parte de los valores y tu tarea es crear una lista de tuplas usando la comprensión de listas que 
+# contenga la etiqueta y el valor de la glucemia en cada tupla.
+
+glicemia = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62, 86, 96, 103, 88, 155, 52, 89, 73]
